a63c4b7a56d67fc3e47fdb2f2f660e4ec0fa5507
Aseempaul007 <aseempal007@gmail.com>
2022-10-29 15:08:48 +0530

    Third commit!

--- app/src/main/java/com/example/converterapp/MainActivity.java
+++ app/src/main/java/com/example/converterapp/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.converterapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        EditText editText;
+        Button button;
+        TextView textView;
+
+        editText =  findViewById(R.id.et1);
+        button = findViewById(R.id.btn1);
+        textView = findViewById(R.id.tv1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText(kiloGramToGram(editText.getText().toString()));
+            }
+        });
+    }
+
+
+    public String kiloGramToGram(String s){
+
+        double val = Double.parseDouble(s);
+        val=val*1000;
+        String str = String.valueOf(val);
+        return str;
     }
 }
